c0afae031d839f56637a6650e389bfd7a85ab9b9
vrajesh007 <43109780+vrajesh007@users.noreply.github.com>
2019-04-24 20:27:29 +0530

    .

--- GenDrug Admin Panel/uploadprescription-api.php
+++ GenDrug Admin Panel/uploadprescription-api.php
@@ -5,15 +5,24 @@ $response = array();
 //Tables Fields 
 //Check Fields are Empty or Not 
 //Only Use Required Fields 
-if (isset($_POST['Pinsert_name']) && !empty($_POST['Pinsert_name']) && isset($_POST['Pinsert_photo']) && !empty($_POST['Pinsert_photo']) && isset($_POST['Pinsert_email']) && !empty($_POST['Pinsert_email'])) {
+if (isset($_POST['Pinsert_name']) && !empty($_POST['Pinsert_name']) && isset($_POST['Pinsert_email']) && !empty($_POST['Pinsert_email'])) {
 
     //Stores Values in Variable
 $a=mysqli_real_escape_string($con,$_POST['Pinsert_name']);
-$b=mysqli_real_escape_string($con,$_POST['Pinsert_photo']);
+//$b=mysqli_real_escape_string($con,$_POST['Pinsert_photo']);
 $c=mysqli_real_escape_string($con,$_POST['Pinsert_email']);
 
+$photo = "upload/".$_FILES['Pinsert_photo']['name'];
 
-    $insertquery = mysqli_query($con, "insert into prescriptioninsert (`Pinsert_id`,`Pinsert_name`,`Pinsert_photo`,`Pinsert_email`) values ('','{$a}','{$b}','{$c}')") or die(mysqli_error($con));
+    $lastinsertid = mysqli_insert_id($con);
+	$directory= "upload/";
+	$time = time();
+
+    move_uploaded_file($_FILES['Pinsert_photo']['tmp_name'],"upload/".$_FILES['Pinsert_photo']['name']);
+	
+	$file = "upload/".$_FILES['Pinsert_photo']['name'];
+	
+    $insertquery = mysqli_query($con, "insert into prescriptioninsert (`Pinsert_id`,`Pinsert_name`,`Pinsert_photo`,`Pinsert_email`) values ('','{$a}','{$file}','{$c}')") or die(mysqli_error($con));
     $lastinsertid = mysqli_insert_id($con);
 
     if ($insertquery) {
--- GenDrug Android Studio Project/GenDrug/app/src/main/java/com/newproject/gendrug/uploadprescription.java
+++ GenDrug Android Studio Project/GenDrug/app/src/main/java/com/newproject/gendrug/uploadprescription.java
@@ -1,43 +1,198 @@
 package com.newproject.gendrug;
 
+import android.Manifest;
+import android.app.Dialog;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.media.Image;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Environment;
+import android.os.StrictMode;
+import android.preference.PreferenceManager;
+import android.provider.ContactsContract;
+import android.provider.MediaStore;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
+import com.android.volley.error.VolleyError;
+import com.android.volley.request.SimpleMultiPartRequest;
 import com.android.volley.toolbox.Volley;
 import com.newproject.gendrug.ApiHelper.JsonField;
 import com.newproject.gendrug.ApiHelper.WebURL;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.File;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
 public class uploadprescription extends AppCompatActivity implements View.OnClickListener {
-    EditText enterpres,uploadpres,enteremail;
-    Button  uploadsubmit;
+    EditText enterpres,enteremail;
+    Button  uploadpres,uploadsubmit;
+    ImageView ProfilePreview;
+
+    private String mCameraFileName;
+    private String imagePath;
+    private static final int PICK_IMAGE_REQUEST =001;
+    private static final int CAMERA_REQUEST =002;
+
+    private static final int PERMISSION_REQUEST=100;
+    Dialog ImageChooserDialog;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_uploadprescription);
+        StrictMode.VmPolicy.Builder builder=new StrictMode.VmPolicy.Builder();
+        StrictMode.setVmPolicy(builder.build());
 
         enterpres= (EditText)findViewById(R.id.etenterpres);
-        uploadpres= (EditText) findViewById(R.id.buploadpres);
+        uploadpres=(Button)findViewById(R.id.buploadpres);
         enteremail=(EditText)findViewById(R.id.etenteremail);
         uploadsubmit= (Button)findViewById(R.id.buploadsubmit);
+        ProfilePreview=(ImageView)findViewById(R.id.ProfilePreview);
+
+        uploadpres.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getPermission();
+            }
+        });
 
         uploadsubmit.setOnClickListener(this);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    private void getPermission() {
+        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.M)
+        {
+            if(ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) !=  PackageManager.PERMISSION_GRANTED
+                    && ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) !=PackageManager.PERMISSION_GRANTED
+                    && ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) !=PackageManager.PERMISSION_GRANTED)
+            {
+                ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.CAMERA}, PERMISSION_REQUEST);
+            }else{
+                showOptionPopUp();
+            }
+        }else{
+                showOptionPopUp();
+        }
+    }
+
+    private void showOptionPopUp() {
+        ImageChooserDialog = new Dialog(this);
+        ImageChooserDialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        ImageChooserDialog.setContentView(R.layout.image_chooser_option);
+        TextView tvTakePicture=ImageChooserDialog.findViewById(R.id.tv_choose_image_take_picture);
+        TextView tvPickGallery=ImageChooserDialog.findViewById(R.id.tv_choose_image_from_gallery);
+
+        tvPickGallery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ImageChooserDialog.dismiss();
+                pickImage();
+            }
+        });
+
+        tvTakePicture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ImageChooserDialog.dismiss();
+                takePicture();
+            }
+        });
+        ImageChooserDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        ImageChooserDialog.show();
+        ImageChooserDialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode== CAMERA_REQUEST && resultCode==RESULT_OK){
+            String photoURI = PreferenceManager.getDefaultSharedPreferences(this).getString("USER_PHOTO_FILE","");
+
+            if(photoURI != null){
+                imagePath=photoURI;
+                Log.d("IMAGE_PATH",imagePath);
+                setImage(imagePath);
+            }else{
+                Toast.makeText(this,"Capture image uri not found", Toast.LENGTH_SHORT).show();
+            }
+        }else if(requestCode==PICK_IMAGE_REQUEST&& resultCode== RESULT_OK){
+            if (data !=null){
+                Uri uri = data.getData();
+                String[] projection ={MediaStore.Images.Media.DATA};
+                Cursor cursor = getContentResolver().query(uri, projection,null, null, null);
+                cursor.moveToFirst();
+                int index= cursor.getColumnIndex(projection[0]);
+                imagePath=cursor.getString(index);
+                Log.d("IMAGE_PATH",imagePath);
+                setImage(imagePath);
+            }
+
+        }
+    }
+
+    private void setImage(String imagePath) {
+        Bitmap bitmap = BitmapFactory.decodeFile(imagePath);
+        ProfilePreview.setVisibility(View.VISIBLE);
+        ProfilePreview.setImageBitmap(bitmap);
+    }
+
+    private void takePicture() {
+        if(getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)){
+            Date date=new Date();
+            DateFormat df = new SimpleDateFormat("hh-mm-ss");
+            File storageDirectory = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
+
+            String strPath = storageDirectory.getPath()+"/"+"ImageUploalDemo";
+
+            storageDirectory=new File(strPath);
+            storageDirectory.mkdirs();
+            strPath=storageDirectory.getPath()+"/"+"Report_"+df.format(date)+".jpg";
+            mCameraFileName=storageDirectory.getPath();
+
+            PreferenceManager.getDefaultSharedPreferences(this).edit().putString("USER_PHOTO_FILE", mCameraFileName).apply();
+            Uri outuri = Uri.fromFile(storageDirectory);
+            Intent intent= new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, outuri);
+            startActivityForResult(intent,CAMERA_REQUEST);
+
+        }
+    }
+
+    private void pickImage() {
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(intent, PICK_IMAGE_REQUEST);
     }
 
     @Override
@@ -60,7 +215,7 @@ public class uploadprescription extends AppCompatActivity implements View.OnClic
     }
 
     private void sendUploadRequest() {
-        StringRequest stringRequest=new StringRequest(Request.Method.POST, WebURL.UPLOAD_PRESCRIPTION_URL, new Response.Listener<String>() {
+        SimpleMultiPartRequest stringRequest=new SimpleMultiPartRequest(Request.Method.POST, WebURL.UPLOAD_PRESCRIPTION_URL, new Response.Listener<String>() {
             @Override
             public void onResponse(String response) {
                 parseUploadResponse(response);
@@ -71,22 +226,36 @@ public class uploadprescription extends AppCompatActivity implements View.OnClic
                 error.printStackTrace();
             }
 
-        }){
-            @Override
-            protected Map<String, String> getParams() throws AuthFailureError {
-                Map<String,String> params=new HashMap<>();
-                params.put(JsonField.UPLOAD_NAME, enterpres.getText().toString());
-                params.put(JsonField.UPLOAD_PHOTO, uploadpres.getText().toString());
-                params.put(JsonField.UPLOAD_EMAIL, enteremail.getText().toString());
-                return params;
-            }
-        };
+        });
+        if (imagePath !=null){
+            stringRequest.addFile(JsonField.UPLOAD_PHOTO,imagePath);
+        }
+        stringRequest.addStringParam(JsonField.UPLOAD_NAME, enterpres.getText().toString().trim());
+        stringRequest.addStringParam(JsonField.UPLOAD_EMAIL, enteremail.getText().toString().trim());
+
         RequestQueue requestQueue = Volley.newRequestQueue(uploadprescription.this);
         requestQueue.add(stringRequest);
+
     }
 
     private void parseUploadResponse(String response) {
         Log.d("RESPONSE",response);
+        try {
+            JSONObject jsonObject = new JSONObject(response);
+            int flag = jsonObject.optInt(JsonField.FLAG);
+            if(flag==1){
+             String message = jsonObject.optString(JsonField.MESSAGE);
+             Toast.makeText(this,message,Toast.LENGTH_SHORT).show();
+             finish();
+            }else{
+                String message = jsonObject.optString(JsonField.MESSAGE);
+                Toast.makeText(this,message,Toast.LENGTH_SHORT).show();
+            }
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
 
     }
 
